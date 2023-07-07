c13f0d5f073be5d4651b2202a67342a74290ec54
nukrobzero <nukrobzero@hotmail.com>
2023-07-07 10:42:14 +0700

    fix: input required

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -20,7 +20,7 @@ const submitForm = () => {
         <form @submit.prevent="submitForm" class="flex flex-col space-y-4">
             <label for="message" class="text-xl font-semibold">Message:</label>
             <input type="text" v-model.trim="message.data" placeholder="type..."
-                class="border border-gray-500 rounded-md p-2" />
+                class="border border-gray-500 rounded-md p-2" required />
             <button type="submit" class="bg-blue-600 hover:bg-blue-400 text-white p-2 rounded-md">Submit</button>
         </form>
     </div>
